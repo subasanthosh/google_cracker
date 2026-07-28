b93614a01779b2805c1772a8c7fc976278c34d16
23am065-glitch <23am065@gmail.com>
2026-07-28 13:03:57 +0530

    added db

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,25 @@
+from typing import Dict
+from fastapi import FastAPI,Body
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],    
+    allow_credentials=True,
+    allow_methods=["*"],     
+    allow_headers=["*"],     
+)
 
 @app.get("/")
 def home():
     return {"message": "Google Cracker"}
 
-
+@app.post("/registerinpage")
+async def register(data: dict):
+    print(data)
+    return data
 
